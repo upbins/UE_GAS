3c0e55acb9078aefc9295f1272959da369a0e8e8
upbins <upbins@gmail.com>
2023-10-20 20:29:32 +0800

    modifiers other attrubutes apply

---
+++ Source/GASystem/Private/AbilitySystem/ModMagCalc/MMC_MaxMana.cpp
@@ -0,0 +1,30 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AbilitySystem/ModMagCalc/MMC_MaxMana.h"
+
+#include "AbilitySystem/BaseAttributeSet.h"
+#include "Interaction/CombatInterface.h"
+
+UMMC_MaxMana::UMMC_MaxMana()
+{
+	IntelligenceDef.AttributeToCapture = UBaseAttributeSet::GetIntelligenceAttribute();
+	IntelligenceDef.AttributeSource =  EGameplayEffectAttributeCaptureSource::Target;
+	IntelligenceDef.bSnapshot = false;
+	RelevantAttributesToCapture.Add(IntelligenceDef);
+}
+
+float UMMC_MaxMana::CalculateBaseMagnitude_Implementation(const FGameplayEffectSpec& Spec) const
+{
+	 const FGameplayTagContainer* SourceTags = Spec.CapturedSourceTags.GetAggregatedTags();
+	 const FGameplayTagContainer* TargetsTags = Spec.CapturedSourceTags.GetAggregatedTags();
+	 FAggregatorEvaluateParameters EvaluateParameters;
+	 EvaluateParameters.SourceTags = SourceTags;
+	 EvaluateParameters.TargetTags = TargetsTags;
+	 float Intelligence = 0;
+	 GetCapturedAttributeMagnitude(IntelligenceDef,Spec,EvaluateParameters,Intelligence);
+	Intelligence = FMath::Max<float>(Intelligence,0);
+	ICombatInterface* CombatInterface = Cast<ICombatInterface>(Spec.GetContext().GetSourceObject());
+	const int32 PlayerLevel = CombatInterface->GetObjectLevel();
+	return 50.f + 2.5f * Intelligence + 15.f * PlayerLevel;
+}
